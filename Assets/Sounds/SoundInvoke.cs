ed247557bc6e1c79727f16e92ac2de077269b4b9
Jarred Norris <jarred867@gmail.com>
2018-12-13 13:38:31 +0000

    Final Changes

---
+++ Assets/Sounds/SoundInvoke.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SoundInvoke : MonoBehaviour
+{
+    public AudioClip clip; //make sure you assign an actual clip here in the inspector
+
+    public void playclip()
+    {
+        AudioSource.PlayClipAtPoint(clip, transform.position);
+    }
+}
